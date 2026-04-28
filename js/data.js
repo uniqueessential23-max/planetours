c3bfe648bda5db353ab62e85e630fe73ee6fbf72
uniqueessential23-max <uniqueessential23@gmail.com>
2026-04-28 16:17:52 +0200

    Update hotel listings with Radisson Blu, Pyramid, Palm Africa, and Imperial Plaza

--- js/data.js
+++ js/data.js
@@ -40,27 +40,43 @@ const tourData = [
 const hotelData = [
     {
         id: 1,
-        title: "Juba Grand Palace",
+        title: "Radisson Blu Hotel, Juba",
         city: "Juba Town",
-        price: 150,
-        image: "https://images.unsplash.com/photo-1566073771259-6a8506099945?auto=format&fit=crop&q=80&w=800",
+        price: 250,
+        image: "img/radisson_blu.jpg",
         rating: 5
     },
     {
         id: 2,
+        title: "Pyramid Continental Hotel",
+        city: "Juba Town",
+        price: 210,
+        image: "img/pyramid.jpg",
+        rating: 5
+    },
+    {
+        id: 3,
+        title: "Palm Africa Hotel",
+        city: "Juba",
+        price: 180,
+        image: "img/palm_africa.jpg",
+        rating: 4.5
+    },
+    {
+        id: 4,
+        title: "Imperial Plaza Hotel Juba",
+        city: "Juba Town",
+        price: 165,
+        image: "img/imperial_plaza.jpg",
+        rating: 4
+    },
+    {
+        id: 5,
         title: "Riverside Resort",
         city: "Juba",
         price: 120,
         image: "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?auto=format&fit=crop&q=80&w=800",
         rating: 4
-    },
-    {
-        id: 3,
-        title: "Savanna Lodge",
-        city: "Nimule",
-        price: 90,
-        image: "https://images.unsplash.com/photo-1493246507139-91e8bef99c02?auto=format&fit=crop&q=80&w=800",
-        rating: 4.5
     }
 ];
 
